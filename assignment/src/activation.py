d659f0a484f179109ab31bea9a2447af5c1ac917
Jeremy Maness <jwmanes2@ncsu.edu>
2020-02-11 12:19:47 -0500

    Implemented sigmoid and sigmoid derivative

--- assignment/src/activation.py
+++ assignment/src/activation.py
@@ -4,11 +4,10 @@
 
 import numpy as np
 
-
 def sigmoid(z):
     """The sigmoid function."""
-    return 0
+    return 1 / (1 + np.exp(-z))
 
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
-    return 0
+    return sigmoid(z) * (1 - sigmoid(z))
